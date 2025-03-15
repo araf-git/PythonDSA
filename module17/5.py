cdfead15231d8db2e91d3d7dfaa74d31929dea6e
araf-git <arafatislam722@gmail.com>
2025-03-15 19:14:09 +0600

    updated

--- module17/5.py
+++ module17/5.py
@@ -17,38 +17,3 @@ bubble_sort(arr)
 print(arr)
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# full dry run of Bubble Sort for arr = [5, 1, 3, 4, 2]:
-
-# Initial Array:
-# [5, 1, 3, 4, 2]
-
-# Pass 1 (i = 0, j = 0 to 3)
-# Compare 5 & 1 → Swap → [1, 5, 3, 4, 2]
-# Compare 5 & 3 → Swap → [1, 3, 5, 4, 2]
-# Compare 5 & 4 → Swap → [1, 3, 4, 5, 2]
-# Compare 5 & 2 → Swap → [1, 3, 4, 2, 5]
-# Pass 2 (i = 1, j = 0 to 2)
-# Compare 1 & 3 → No swap → [1, 3, 4, 2, 5]
-# Compare 3 & 4 → No swap → [1, 3, 4, 2, 5]
-# Compare 4 & 2 → Swap → [1, 3, 2, 4, 5]
-# Pass 3 (i = 2, j = 0 to 1)
-# Compare 1 & 3 → No swap → [1, 3, 2, 4, 5]
-# Compare 3 & 2 → Swap → [1, 2, 3, 4, 5]
-# Pass 4 (i = 3, j = 0 to 0)
-# Compare 1 & 2 → No swap → [1, 2, 3, 4, 5]
-# Final Sorted Array:
-# [1, 2, 3, 4, 5] ✅
